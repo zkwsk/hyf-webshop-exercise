439c1079798861024d24b7f00f8047fdadf0f521
rufaidaju <eng.rufaidajumaa@gmail.com>
2017-08-20 15:37:02 +0200

    new webshop

---
+++ webshop.js
@@ -0,0 +1,104 @@
+$(document).ready(function(){
+    $("h1").css("color","purple");
+// $.getJSON( "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function( x ) {
+//         $("#here").text("!!999!");
+//     });
+// function(){
+//   <ul id="in">
+//    <div class ="element">
+//     <li>  <div class="images"><img src="https://d85wutc1n854v.cloudfront.net/live/products/icons/WB0148688.jpg?v=1.0"></div></li>
+//     <li ><span class="price"></span></li>
+//     <li ><span class="purchases"></span></li>
+//     <li ></li>
+//    </div>
+//   </ul>
+// }
+// $.getJSON( "data.json" , function(result){
+
+//       // //  console.log("hello");
+//       // //  var mydata = result.products;
+//       // //  mydata.forEach(console.log(result['products'].id))
+//       // var mydata = result.products;
+//       // mydata.forEach(function (val) {
+//         console.log(result);
+//         var mydata=result.products;
+
+//         // function element(i){
+//         // for (var i in mydata){
+//         // $("#item-name").text(mydata[i].item_name);
+//         // $("ul li:nth-child(3)").text();
+//         // $("ul li:nth-child(4)").text("From: "+mydata[i].country_of_origin);
+//       // }
+//       // }
+// // for(var i=0;i<300;i++){
+// // $("#out ul").append('$<li>'.append(mydata[i].item_name,mydata[i].price<span>));
+// // }
+
+
+
+//   function add (callback){
+//     var newListItem;
+//         for(var i=0;i<300;i++){
+//           newListItem = callback(mydata[i].item_name,mydata[i].price,mydata[i].country_of_origin);
+//           console.log(i);
+//        //  console.log(mydata[i].item_name,mydata[i].price);
+//        // $("#out ul>li:last ").html('<li></li>');
+//           $("#out ul li:last").append(newListItem);
+
+//             // $("<li>").before(callback(mydata[i].item_name,mydata[i].price,mydata[i].country_of_origin)) ;
+//          // $("#out ul").append($("<li>").text(callback(mydata[i].item_name,mydata[i].price,mydata[i].country_of_origin)));
+//                    }
+//      };
+//      function edit(name,price,origin){
+//      return $("#container").text( name+" ,"+"Price: " + price +" $  ,From: "+ origin );
+//       };
+//       add(edit);
+
+
+
+// // $("#out").append($("<li>").text("hello").append($("<li>").text("secoond")));
+
+
+//      //   function add (callback){
+//      //   mydata.forEach(function(product){
+//      //    console.log(product.item_name,product.price);
+
+//      //    $("#out").append($("<li>").append(callback(product.item_name,product.price,product.country_of_origin)));
+//      //        })
+//      // };
+//      // function edit(name,price,origin){
+//      // $("#details").text( name+" ,"+"Price: " + price +" $  ,From: "+ origin );
+//      //  };
+//      //  add(edit);
+
+
+
+
+//       // mydata.forEach(function(product){
+//       //   console.log(product.item_name,product.price)
+//       //   $("ul li:nth-child(2)").text(product.item_name +" ,"+"Price: " + product.price +" $  ,From: "+ product.country_of_origin );
+//       //  //   }
+
+//       // })
+//     //   function modi(i){
+//     //     $("ul li:nth-child(2)").text(mydata[i].item_name +" ,"+"Price: " + mydata[i].price +" $  ,From: "+ mydata[i].country_of_origin );
+//     //   }
+//     //   function add (callback)
+//     // {  mydata.forEach(function(val){
+//     //     $("#out ul li").append(callback(val));
+//     //   });}
+//     //   add(modi);
+
+//       // mydata.forEach(function(i){
+//       //   $("ul li:nth-child(2)").text(mydata[i].item_name+" :"+"Price: " + mydata[i].price +" $  From: "+ mydata[i].country_of_origin + mydata[i].item_name);
+//       //
+//       // })
+
+//         // $("ul").append('<li>','<span class="price">','20$','</span>','</li>');
+//       //  $("#col1").append('<li><div class="element" ><div class="col-md-3 col-sm-offset-6 col-xs-7 im-centered "><div id="item-name"></div><div class="images"><img  id="im" src="https://d85wutc1n854v.cloudfront.net/live/products/icons/WB0D7PKKG.jpg?v=1.5"></div> <span class="price">',mydata[0].price,'</span><span class="purchases">10000000</span></div></div></li>');
+//         // $("#col2").append('<li>',{}).append('<div class="col-md-3 col-sm-offset-6 col-xs-7 im-centered element">',{}).append('<div id="item-name"></div>','<div class="images">').append('<img  id="im" src="https://d85wutc1n854v.cloudfront.net/live/products/icons/WB0D7PKKG.jpg?v=1.5">').append()'</div>',' <div class="price">','1111$','</div>','<span class="purchases">10000000</span>','</div>','</div>','<li>');
+
+      
+   // });
+
+});
